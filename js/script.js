652764b96137e1613714a2b24cd8206eb15543c5
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-05-30 17:05:27 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -8,13 +8,35 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit-x-yy-JS/sw.js", {
-    scope: "/ICS2O-Unit-x-yy-JS/",
+  navigator.serviceWorker.register("/Assignment-6", {
+    scope: "/Assignment-6/",
   });
 }
 
-/**
- * Input
- */
-function myButtonClicked() {
+const getImage = async (URLAddress) => {
+  try {
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData)
+    document.getElementById("api-image").innerHTML =
+    '<img src="' + 
+      jsonData.url + 
+      '" alt="API image" class="center" ' +
+      '>'
+    if (jsonData.artist_url != "none") {
+      document.getElementById("image-artist").innerHTML =
+      "<p>Artist: " +
+      '<a href="' +
+      jsonData.artist_url +
+      '">' +
+      jsonData.artist +
+      "</a>"
+  } else {
+    document.getElementById("image-artist").innerHTML = "<p>Artist: unknown</p>"
+  }
+  } catch (err) {
+    console.log(err)
+  }
+}
 
+getImage("https://api.genshin.dev")
